0a1c63ee756e5fc7b218360d8fc830a4c2f8f742
Shaswat Rungta <shaswatrungta@hotmail.com>
2019-04-23 01:52:48 -0700

    [Sub] Check point change in sub (#9)

--- src/index.js
+++ src/index.js
@@ -2,4 +2,6 @@ console.log("I am a barbie girl.")
 console.log("In the barbie world.")
 console.log("Life in plastic.It's fantastic.")
 console.log("A...O..Barbie..lets go party.")
+console.log("Checkpoint.")
+
 
