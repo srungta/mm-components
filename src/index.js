ed258741c592e6d4ca928cec9397517db201250d
Shaswat Rungta <shaswatrungta@hotmail.com>
2019-04-23 03:14:14 -0700

    Revert "Contrib from ext team (#5)" (#6)
    This reverts commit eee8fbc70af95af0c7e1f0c0ceb9e85cf868ba5b.

--- src/index.js
+++ src/index.js
@@ -3,5 +3,5 @@ console.log("In the barbie world.")
 console.log("Life in plastic.It's fantastic.")
 console.log("A...O..Barbie..lets go party.")
 console.log("Checkpoint.")
-console.log("External team contributed this.")
+
 
